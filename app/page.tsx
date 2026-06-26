c4f93b3d446cc781c077b7492d34183d2344714f
Abigail Marte <blujayabby@gmail.com>
2026-06-27 00:28:16 +0800

    feat: add ReaderLove testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,6 @@ export default function HomePage() {
       <PopularCategories />
       <ContinueReading />
       <WhyKatha />
-      <WhyKatha />
       <ReaderLove />
       <FeaturedAuthors />
       <FinalCTA />
